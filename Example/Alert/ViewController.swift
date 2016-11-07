e2e1fe88be08963912d18e179363a3054a4cb19f
Stan Liu <lyc2345@gmail.com>
2016-11-07 17:14:43 +0800

    add example

--- Example/Alert/ViewController.swift
+++ Example/Alert/ViewController.swift
@@ -1,24 +1,90 @@
 //
 //  ViewController.swift
-//  Alert
+//  AlertTest
 //
-//  Created by Stan Liu on 11/07/2016.
-//  Copyright (c) 2016 Stan Liu. All rights reserved.
+//  Created by Stan Liu on 07/11/2016.
+//  Copyright © 2016 Stan Liu. All rights reserved.
 //
 
 import UIKit
+import Alert
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+  var alertController: UIAlertController!
+  @IBOutlet weak var buttonAmountLabel: UILabel!
+  @IBOutlet weak var textfieldAmountLabel: UILabel!
+  
+  var btn: Int! = 0
+  var tex: Int! = 0
+  
+  override func viewDidLoad() {
+    buttonAmountLabel.text = "0"
+    textfieldAmountLabel.text = "0"
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+  }
+  
+  @IBAction func increaseButton(_ sender: Any) {
+    
+    btn = btn + 1
+    buttonAmountLabel.text = "\(btn!)"
+  }
+  
+  
+  @IBAction func decreaseButton(_ sender: Any) {
+    
+    if btn == 0 { return }
+    
+    btn = btn - 1
+    buttonAmountLabel.text = "\(btn!)"
+  }
+  
+  
+  @IBAction func increaseTextfield(_ sender: Any) {
+    
+    tex = tex + 1
+    textfieldAmountLabel.text = "\(tex!)"
+  }
+  
+  
+  @IBAction func decreaseTextfield(_ sender: Any) {
+    
+    if tex == 0 { return }
+    
+    tex = tex - 1
+    textfieldAmountLabel.text = "\(tex!)"
+  }
+  
+  
+  @IBAction func presentAlert(_ sender: Any) {
+    
+    if btn == 0 {
+      
+      Alert.with(title: "Warning", message: "Button at least 1", style: .alert).bind(button: "I know!", style: .default, completion: nil).show()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    alertController = Alert.with(title: "Example", message: "I am a example", style: .alert)
+    
+    for i in 0 ..< btn {
+      
+      let _ = alertController.bind(button: "Button \(i + 1)", style: .default) {
+        (action) in
+      }
     }
-
+    
+    for j in 0 ..< tex {
+      
+      let _ = alertController.bind(textfield: "Textfield \(j + 1)", placeholder: "I am a textfield", secure: false, returnHandler: { (textfield) in
+        
+      })
+    }
+    
+    alertController.show()
+    
+  }
+  
 }
-
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,6 +4,47 @@ import Quick
 import Nimble
 import Alert
 
+class Tests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    XCUIApplication().launch()
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    
+    let app = XCUIApplication()
+    
+    app.buttons["+"].tap()
+    
+    app.buttons["Show"].tap()
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    XCTAssert(true, "Pass")
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure() {
+      // Put the code you want to measure the time of here.
+    }
+  }
+  
+}
+
+
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
         describe("these will fail") {
@@ -32,13 +73,14 @@ class TableOfContentsSpec: QuickSpec {
 
                 it("will eventually pass") {
                     var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
+                  
+                  DispatchQueue.main.async {
+                    
+                    time = "done"
+                  }
 
                     waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
+                        Thread.sleep(forTimeInterval: 0.5)
                         expect(time) == "done"
 
                         done()
