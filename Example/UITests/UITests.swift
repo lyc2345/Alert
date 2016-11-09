e946c093c09dd7ae7884fb90173ec332076ec5c1
Stan Liu <lyc2345@gmail.com>
2016-11-09 10:44:50 +0800

    edit podfile to support Tests, edit UITexts.swift

--- Example/UITests/UITests.swift
+++ Example/UITests/UITests.swift
@@ -1,13 +1,19 @@
-// https://github.com/Quick/Quick
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Stan Liu on 09/11/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
 
-import Quick
-import Nimble
+import XCTest
 import Alert
 
-class Tests: XCTestCase {
+class UITests: XCTestCase {
   
   override func setUp() {
     super.setUp()
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
     // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -16,10 +22,23 @@ class Tests: XCTestCase {
     XCUIApplication().launch()
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    
+    let app = XCUIApplication()
+    
+    app.buttons["+"].tap()
+    
+    app.buttons["Show"].tap()
   }
   
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
+  
+  func testExample() {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  
 }
